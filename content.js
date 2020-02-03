f59bff95139ae5d488f7aade0846d45e5d36e58e
ettorni <46283098+ettorni@users.noreply.github.com>
2020-02-03 03:11:18 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,24 @@
+
+function message_1() {
+    alert("You've been on this website for 10 minutes")
+}
+
+function message_2() {
+    alert("You should probably get off now it's been an hour")
+}
+
+function message_3() {
+    alert("BAD! ITS BEEN TWO HOURS")
+}
+
+setTimeout(message_1, 600000)
+
+setTimeout(message_2, 3600000)
+
+setTimeout(message_3, 7200000)
+
+
+
+
+
+
